d61f71315e2a053ae83a00b02b55a96b64d846aa
RhytmicRider <devos.alex.11@gmail.com>
2025-05-17 10:46:51 +0200

    blob

--- startcode/main.py
+++ startcode/main.py
@@ -1,7 +1,49 @@
 import pygame
 import time
 from snake import Snake
-from food import Food
+# from food import F
+from snake import veld_groote
+
+breedte = 800
+hoogte = 600
+pygame.init()
+kleur_achtergrond = (0, 0, 0)
+
+venster = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Snake")
+
+def game_lus(veld_grootte = 20):
+    snake = Snake(breedte/2, hoogte/2)
+
+    game_over = False
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT and snake.x_verandering == 0:
+                    snake.x_verandering = -veld_grootte
+                    snake.y_verandering = 0
+                elif event.key == pygame.K_RIGHT and snake.x_verandering == 0:
+                    snake.x_verandering = veld_grootte
+                    snake.y_verandering = 0
+                elif event.key == pygame.K_UP and snake.y_verandering == 0:
+                    snake.y_verandering = -veld_grootte
+                    snake.x_verandering = 0
+                elif event.key == pygame.K_DOWN and snake.y_verandering == 0:
+                    snake.y_verandering = veld_grootte
+                    snake.x_verandering = 0
+
+        snake.beweeg()
+        venster.fill((0, 0,0))
+
+        snake.teken(venster)
+        pygame.display.update()
+        time.sleep(1/5)
+
+
+game_lus()
+
 
 # kleuren
 
@@ -18,3 +60,12 @@ from food import Food
 # Hoofdloop van het spel
 
 # Start de hoofdloop van het spel
+
+
+
+
+
+
+
+
+
--- startcode/snake.py
+++ startcode/snake.py
@@ -1,12 +1,40 @@
 import pygame
 
+kleur_slang = (0, 255, 0)
+
+veld_groote = 20
+
 class Snake:
-    def __init__(self):
+    def __init__(self, startx, starty):
+        self.x = startx
+        self.y = starty
+
+        self.lijst_slang = [[startx, starty]]
+        self.lengte_slang = 1 # lr,(self.lijst_slang)
+        self.x_verandering = 0
+        self.y_verandering = 0
+
 
     def beweeg(self):
+        self.x += self.x_verandering
+        self.y += self.y_verandering
+        slang_kop =[self.x, self.y]
+        self.lijst_slang.append(slang_kop)
+        if len(self.lijst_slang) > self.lengte_slang:
+            del self.lijst_slang[0]
 
     def teken(self, venster):
+        for segment in self.lijst_slang:
+            pygame.draw.rect(venster, kleur_slang, pygame.Rect(segment[0], segment[1],veld_groote,veld_groote))
+
+
+
+
+
+
+
 
     def is_buiten_veld(self, breedte, hoogte):
-
+        pass
     def raakt_zichzelf(self):
+        pass
